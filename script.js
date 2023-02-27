0f859cac95d08e48175d0e52ff9da0df42debcf5
Piyush Shukla <panditofcodes@gmail.com>
2023-02-27 23:23:45 +0530

    JS is Completed

---
+++ script.js
@@ -0,0 +1,46 @@
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+const form = document.querySelector("form")
+const search = document.querySelector("#search")
+const weather = document.querySelector("#weather")
+    // const API = `https://api.openweathermap.org/data/2.5/weather?
+    // q=${city}&appid=${API_KEY}&units=metric`
+    // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+const getWeather = async(city) => {
+    weather.innerHTML = `<h2> Loading... <h2>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json()
+    return showWeather(data)
+}
+
+const showWeather = (data) => {
+    if (data.cod == "404") {
+        weather.innerHTML = `<h2> City Not Found <h2>`
+        return;
+    }
+    var tempWeather = data.weather[0].main
+    
+    switch (tempWeather) {
+        case tempWeather=="rain":
+            document.body.style.background = "#f3f3f3 url('image/rain.png')";
+            break;
+    
+        default:
+            document.body.style.background = "#f3f3f3 url('https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80')";
+            break;
+    }
+    weather.innerHTML = `
+        <div class="temp">
+            <h2>${data.main.temp} ℃</h2>
+            <h4> ${data.weather[0].main} </h4>
+        </div>
+    `
+}
+
+form.addEventListener(
+    "submit",
+    function(event) {
+        getWeather(search.value)
+        event.preventDefault();
+    }
+)
